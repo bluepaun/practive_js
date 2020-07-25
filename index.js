d240be118101af4c662f3d355db628004a216f08
bluepaun <bluepaun@gmail.com>
2020-07-25 20:40:07 +0900

    document window event

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-const title = document.getElementById("title");
-const title2 = document.querySelector("#title");
-const title3 = document.querySelector(".title");
-title.style.color = "red";
-document.title = "I own you now";
-console.dir(document)
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick(){
+    const currentColor = title.style.color;
+    if( currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
+
+}
+
+
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("mouseenter", handleClick);
+}
+
+init();
+
+function handleOffline(){
+    console.log("offline");
+}
+
+function handleOnline(){
+    console.log("online");
+}
+
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
