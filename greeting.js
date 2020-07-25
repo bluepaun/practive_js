6148d9ee4125c1d61dde65f080f6a3d1bdc4d94a
bluepaun <bluepaun@gmail.com>
2020-07-26 01:31:43 +0900

    save name 2

--- greeting.js
+++ greeting.js
@@ -5,6 +5,21 @@ greeting = document.querySelector(".js-greeting");
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+}
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
@@ -14,7 +29,7 @@ function paintGreeting(text){
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-        //
+        askForName();
     } else {
         paintGreeting(currentUser);
     }
@@ -22,6 +37,7 @@ function loadName(){
 
 function init(){
     loadName();
+    form.addEventListener("submit", handleSubmit);
 }
 
 init();
